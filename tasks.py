f518dfeaae6ff8aaee1f09e8ffeeb3574659ac04
RCar4 <ctw21.ruairi.cotter@corketb.ie>
2025-09-08 13:57:45 +0100

    Add files via upload

---
+++ tasks.py
@@ -0,0 +1,94 @@
+print("###### ###### ######")
+print("    ## ##     ##    ")
+print("###### ###### ######")
+print("    ## ##  ##     ##")
+print("###### ###### ######")
+
+
+#dislikes: incorrect spelling, wrong indentations, and wrong symbols.
+#likes: showing your code, /t, and matching brackets.
+print(".")
+print(".")
+print(".")
+
+
+goals = 0
+goals = goals + 1
+print("the value of goals is", goals)
+answer = 1+2
+
+print(".")
+print(".")
+print(".")
+
+personName = "murphy"
+favouriteColour = "green"
+print("hi", personName, "- your favourite colour is ", favouriteColour)
+print("goodbye", personName)
+
+print(".")
+print(".")
+print(".")
+
+accountBalance = 1000
+withdrawalAmount = 600
+accountBalance = accountBalance - withdrawalAmount
+print("account balance is", accountBalance)
+
+print(".")
+print(".")
+print(".")
+
+year = int (input ("enter the current year: "))
+age = int (input ("what age will you be at the end of the year: "))
+print("you were born in", year - age)
+
+print(".")
+print(".")
+print(".")
+
+
+print("task 5")
+
+
+
+centigrade = float (input("enter the centigrade value: "))
+fahrenheit = 9/5 * centigrade + 32
+print(centigrade, "degrees C equals", fahrenheit, "degrees F")
+
+print(".")
+print(".")
+print(".")
+
+principle = input("enter principle: ")
+principle = float(principle)
+
+rate = input("Enter rate: ")
+rate = float(rate)
+
+time = input("Enter time in years: ")
+time = float(time)
+
+amount = principle * rate * time
+
+print("the interest amount is", amount)
+
+
+print(".")
+print(".")
+print(".")
+
+
+
+
+
+runningTotal = 0
+
+price1 = 10
+runningTotal = runningTotal + price1
+price2 = 14
+runningTotal = runningTotal + price2
+price3 = 6
+runningTotal = runningTotal + price3
+
+print("Total amount is", runningTotal)
