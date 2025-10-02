f7f6ab62221ba9e41e82d76ee5aedf28cd75f23a
RCar4 <ctw21.ruairi.cotter@corketb.ie>
2025-10-02 10:29:45 +0100

    Add files via upload

---
+++ password checker.py
@@ -0,0 +1,21 @@
+def capital():
+    capital = (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z)
+
+capital()
+
+def password():
+    password = input("enter password: ")
+
+password()
+
+def charecter_check():
+    if password = 7
+        return capital_check()
+    else:
+        print("wrong password")
+
+def capital_check():
+    if password = capital()
+        print("...")
+    else:
+        print("wrong")
