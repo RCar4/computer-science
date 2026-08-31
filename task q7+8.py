e17aebfac3f127e20e115a43066078c3f4e62e9b
RCar4 <ctw21.ruairi.cotter@corketb.ie>
2026-08-31 17:47:23 +0100

    Add files via upload

---
+++ task q7+8.py
@@ -0,0 +1,20 @@
+print("task 7")
+
+length = float(input("what is the length of your rectangle: "))
+width = float(input("what is the width of your rectangle: "))
+
+answer = length*width
+
+if length==width:
+    print("thats a square")
+
+
+print("the area is:", answer)
+
+print("task 8")
+tot = float(input("what is the total amount: "))
+per = float(input("what is the percentage: "))
+
+answer2 = tot/per
+
+print(answer2)
